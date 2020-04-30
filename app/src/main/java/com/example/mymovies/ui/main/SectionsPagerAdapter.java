0e01988c5c4ac7497878e8961485be36d72ab62b
Orangestreak <rjcj0421@gmail.com>
2020-04-30 00:57:31 -0400

    Added UI for the fragments and activities

--- app/src/main/java/com/example/mymovies/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/mymovies/ui/main/SectionsPagerAdapter.java
@@ -2,6 +2,7 @@ package com.example.mymovies.ui.main;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
@@ -9,6 +10,8 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
 import com.example.mymovies.R;
+import com.example.mymovies.FavoriteFragment;
+import com.example.mymovies.SearchFragment;
 
 /**
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
@@ -21,26 +24,46 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     private final Context mContext;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
-        super(fm);
+        super(fm, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
         mContext = context;
     }
 
+    @NonNull
     @Override
     public Fragment getItem(int position) {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+        Fragment fragment = null;
+        switch (position) {
+            case 0:
+                fragment = new SearchFragment();
+                break;
+            case 1:
+                fragment = new FavoriteFragment();
+                break;
+        }
+        return fragment;
     }
 
     @Nullable
     @Override
     public CharSequence getPageTitle(int position) {
+        /*Drawable myDrawable;
+        myDrawable = mContext.getResources().getDrawable(TAB_ICONS[position],null);
+        SpannableStringBuilder sb = new SpannableStringBuilder(" "); // space added before text for convenience
+        try {
+            myDrawable.setBounds(5, 5, myDrawable.getIntrinsicWidth(), myDrawable.getIntrinsicHeight());
+            ImageSpan span = new ImageSpan(myDrawable, DynamicDrawableSpan.ALIGN_BASELINE);
+            sb.setSpan(span, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            return sb;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return mContext.getResources().getString(TAB_TITLES[position]);
+        }*/
         return mContext.getResources().getString(TAB_TITLES[position]);
     }
 
     @Override
     public int getCount() {
-        // Show 2 total pages.
+        // Show 4 total pages.
         return 2;
     }
 }
